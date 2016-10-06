54f566ccf69c9ee5a0977bea90ff4c9e4806a850
Claudia Navarrete <claudia.navarrete@yale.edu>
2016-10-06 18:14:49 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
+    app.get('/', function(request, response){
     response.send('Hello MGT-656!');
+});    
+    app.get('/nickname', function(request, response){
+    response.send('faithful-lark');
 });
 
 app.listen(process.env.PORT || 4000);
